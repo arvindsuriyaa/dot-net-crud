777bbc918cae41844784b6ba0701ee89de729d8f
arvindsuriyaa <arvinds2605@gmail.com>
2022-08-17 22:04:22 +0530

    first commit

---
+++ bookStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,120 @@
+﻿using bookStoreWeb.Data;
+using bookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace bookStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET Book
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Categories;
+
+            return View(categoryList);
+        }
+
+        //Get Book
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Book
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order cannot be Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category successfully created";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get Edit Book
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFrmDb = _db.Categories.Find(id);
+
+            return View(categoryFrmDb);
+        }
+
+        //Post Edited Book
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            var userDetails = _db.Categories.Where(u => u.Name == obj.Name).ToList();
+            if (userDetails.Count>=2)
+            {
+                ModelState.AddModelError("name", "Name already exist, Choose different Name");
+
+            }
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Display Order cannot be Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["Success"] = "Category successfully edited";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //Get Edit Book
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFrmDb = _db.Categories.Find(id);
+
+            return View(categoryFrmDb);
+        }
+
+        //Post Edited Book
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var userDetails = _db.Categories.Find(id);
+            if (userDetails == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(userDetails);
+            _db.SaveChanges();
+            TempData["Success"] = "Category successfully deleted";
+            return RedirectToAction("Index");
+           
+        }
+
+    }
+}
---
+++ bookStoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using bookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookStoreWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
